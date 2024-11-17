f91b67dac8d11b46b8edd69288ff0a5ef5fbb33c
A2AFelsen <michael.t.huffer@gmail.com>
2024-11-16 20:58:35 -0500

    Return msg from add_new_character()

--- create_character.py
+++ create_character.py
@@ -4,4 +4,7 @@ import database_manager
 def create(username, campaign, campaign_password, character_name, character_health):
     if not database_manager.login_campaign(campaign, campaign_password):
         return "Campaign Password incorrect!"
-    return database_manager.add_new_character(username, campaign, character_name, character_health)
+    output, msg = database_manager.add_new_character(username, campaign, character_name, character_health)
+    if not output:
+        return msg
+    return msg
