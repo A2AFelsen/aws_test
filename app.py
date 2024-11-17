89cd3407ed16257ab0f0cbbaf6c781cb110cb17d
A2AFelsen <michael.t.huffer@gmail.com>
2024-11-17 08:05:34 -0500

    always return npc message

--- app.py
+++ app.py
@@ -144,6 +144,8 @@ def npc_create_submit():
         return npc_create.create(npc_name, npc_health)
     elif action == 'main_menu':
         return render_template('main_menu.html', username=username)
+    else:
+        return "HERE?"
 
 
 @app.route('/play_submit', methods=['POST'])
